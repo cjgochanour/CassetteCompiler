75b867555688d5e2b2cc484dc52397716c13b3ca
cjgochanour <cjgochanour@gmail.com>
2022-04-01 16:16:01 -0500

    added user and cassette models

---
+++ CassetteCompiler/CassetteCompiler/Models/Cassette.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CassetteCompiler.Models
+{
+    public class Cassette
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int Year { get; set; }
+        public string Notes { get; set; }
+        public List<string> Genres { get; set; } 
+    }
+}
