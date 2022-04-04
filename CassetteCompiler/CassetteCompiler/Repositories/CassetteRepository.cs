16d6e7bf4760ff5f511c134f674ee3b3925b9645
cjgochanour <cjgochanour@gmail.com>
2022-04-03 22:38:38 -0500

    added getallusers and getbyid in usersrepository

---
+++ CassetteCompiler/CassetteCompiler/Repositories/CassetteRepository.cs
@@ -0,0 +1,11 @@
+﻿using CassetteCompiler.Models;
+using System.Collections.Generic;
+
+namespace CassetteCompiler.Repositories
+{
+    public class CassetteRepository
+    {
+        public List<Cassette> GetAllCassettes();
+        public Cassette GetByUserId(int id);
+    }
+}
