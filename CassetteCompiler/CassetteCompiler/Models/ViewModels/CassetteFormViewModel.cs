499e4d57b2c7b94e6599503979f6a3195bfa00f3
cjgochanour <cjgochanour@gmail.com>
2022-04-05 14:12:17 -0500

    added cassetteformviewmodel

---
+++ CassetteCompiler/CassetteCompiler/Models/ViewModels/CassetteFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace CassetteCompiler.Models.ViewModels
+{
+    public class CassetteFormViewModel
+    {
+        public Cassette Cassette { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
