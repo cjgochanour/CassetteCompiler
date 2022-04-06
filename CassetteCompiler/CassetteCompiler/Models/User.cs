f3b5cb27c72917976499fb22681091906302ef6e
cjgochanour <cjgochanour@gmail.com>
2022-04-06 14:28:44 -0500

    email addresses must be emails

--- CassetteCompiler/CassetteCompiler/Models/User.cs
+++ CassetteCompiler/CassetteCompiler/Models/User.cs
@@ -1,9 +1,13 @@
-﻿namespace CassetteCompiler.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CassetteCompiler.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
